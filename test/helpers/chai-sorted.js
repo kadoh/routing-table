4de9d219f2894777fe0fda7303efcf6642b30222
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2013-12-08 21:54:43 +0100

    Refactor KBucket class + test

---
+++ test/helpers/chai-sorted.js
@@ -0,0 +1,17 @@
+var crypto = require('../../lib/crypto');
+
+function helperChai(chai, utils) {
+
+  function assertSorted(compareFn) {
+    compareFn = compareFn || function(a, b) { return a - b; };
+
+    var obj = utils.flag(this, 'object');
+    for (var i = 0, l = obj.length; i < l - 1; i++) {
+      this.assert(compareFn(obj[i], obj[i+1]) < 0, "expected array to be sorted " + utils.inspect(obj));
+    }
+  }
+
+  chai.Assertion.addMethod('sorted', assertSorted);
+}
+
+module.exports = helperChai;
