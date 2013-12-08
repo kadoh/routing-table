826ca9c02ffcad234124698b75e59652367c8071
Lachèze Alexandre <alexandre.lacheze@gmail.com>
2013-12-08 22:26:33 +0100

    Add npm package.json and index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = require('./lib/routing-table');
+module.exports.Peer = require('./lib/peer');
+module.exports.BootstrapPeer = require('./lib/bootstrap-peer');
+module.exports.KBucket = require('./lib/kbucket');
+module.exports.PeerArray = require('./lib/peerarray');
